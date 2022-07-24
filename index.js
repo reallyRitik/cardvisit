df097e590ace67a81accd5726bed465b8761d41d
RItik <reallyhritik@gmail.com>
2022-07-24 17:43:02 +0530

    added all the file

---
+++ index.js
@@ -0,0 +1,21 @@
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close")[0];
+ 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+  });
+});
